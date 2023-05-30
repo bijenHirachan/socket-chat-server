4f4818bbb7245790e5198ebfbc2b8b7ff28856de
Bijen Hirachan <bijen.hirachan@gmail.com>
2023-05-30 17:02:17 +0200

    Frontend url updated

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
   },
 });
